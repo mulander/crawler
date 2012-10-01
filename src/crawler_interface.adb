341dd56561600caab739a28a4d75084e6cf711b3
mulander <netprobe@gmail.com>
2012-10-02 01:39:40 +0200

    Added license header to new files

--- src/crawler_interface.adb
+++ src/crawler_interface.adb
@@ -1,3 +1,7 @@
+-- Copyright (c) 2012, mulander <netprobe@gmail.com>
+-- All rights reserved.
+-- Use of this source code is governed by a BSD-style license that can be
+-- found in the LICENSE file.
 with Terminal_Interface.Curses;
 
 package body Crawler_Interface is
--- src/crawler_interface.ads
+++ src/crawler_interface.ads
@@ -1,3 +1,7 @@
+-- Copyright (c) 2012, mulander <netprobe@gmail.com>
+-- All rights reserved.
+-- Use of this source code is governed by a BSD-style license that can be
+-- found in the LICENSE file.
 with Ada.Finalization;
 package Crawler_Interface is
    type Screen is new Ada.Finalization.Limited_Controlled with null record;
