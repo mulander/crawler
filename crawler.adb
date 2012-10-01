8c78436dc549026a2f9f0f95cd3f5c9fdc2dbf9d
mulander <netprobe@gmail.com>
2012-10-01 20:51:56 +0200

    Second snippet - forgot to include in the last commit.

--- crawler.adb
+++ crawler.adb
@@ -2,12 +2,42 @@ with Terminal_Interface.Curses;
 
 procedure Crawler
 is
-	package Curses renames Terminal_Interface.Curses;
-	Key : Curses.Real_Key_Code;
+   package Curses renames Terminal_Interface.Curses;
+
+   Key : Curses.Key_Code;
+   Cursor_Visibility : Curses.Cursor_Visibility := Curses.Invisible;
+   -- Define the main character initial position and symbol
+   Row : Curses.Line_Position := 10;
+   Col : Curses.Column_Position := 10;
+   Main_Character : Character:= '@';
 begin
-	Curses.Init_Screen; --initscr;
-	Curses.Clear; --clear;
-	--printw("Seems that you can use ncurses...\nPress any key to exit!");
-	Key := Curses.Get_Keystroke; --getch();
-	Curses.End_Windows; --endwin();
+   -- Initialize ncurses
+   Curses.Init_Screen; --initscr;
+   Curses.Clear; --clear;
+   Curses.Set_Echo_Mode (False);
+   Curses.Set_Cbreak_Mode (True);
+   Curses.Set_Keypad_Mode;
+   Curses.Set_Cursor_Visibility (Cursor_Visibility);
+
+   -- Printw is not ported binded by design
+   -- Print a welcome message on the screen
+   Curses.Add (Str => "Welcome to RR game." & Standard.ASCII.LF);
+   Curses.Add (Str => "Press any key to start." & Standard.ASCII.LF);
+   Curses.Add (Str => "If you want to quit press ""q"" or ""Q""");
+
+   -- Start the game loop
+   loop
+      -- Wait until the user presses a key
+      Key := Curses.Get_Keystroke; --getch();
+      -- Clear the screen
+      Curses.Clear;
+
+      case Key is
+      when  Curses.Real_Key_Code(Character'Pos('q')) | Curses.Real_Key_Code(Character'Pos('Q')) =>
+         exit;
+      when others => -- If the user choses to stay, show the main character at position (Row,Col)
+         Curses.Add (Line => Row, Column => Col, Ch => Main_Character);
+      end case;
+   end loop;
+   Curses.End_Windows; --endwin();
 end Crawler;
